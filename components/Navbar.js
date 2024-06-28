9f86ec5db02f46a16f58eac16bfcd8f342d19f26
muhammadsaad25 <muhammadsaad2522@gmail.com>
2024-06-28 12:40:54 +0500

    crud complete but more functions remainging

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { FiPlusCircle, FiHome } from 'react-icons/fi'
 
 function Navbar() {
   return (
-    <nav className="flex gap-6 justify-between items-center cursor-pointer">
+    <nav className="flex gap-6 justify-between items-center cursor-pointer px-4">
       <Link className="flex items-center gap-1" href={'/'}>
         <FiHome /> <sapn>Home</sapn>
       </Link>
